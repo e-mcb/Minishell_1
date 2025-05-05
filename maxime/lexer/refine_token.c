b0e6ff945b3512012d738a674c1b7adae9c0d1a6
Maxime Zutter <mzutter@k0r4p2.42mulhouse.fr>
2025-05-05 18:47:38 +0200

    forgor refine token

---
+++ maxime/lexer/refine_token.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   refine_token.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzutter <mzutter@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 17:12:13 by mzutter           #+#    #+#             */
+/*   Updated: 2025/05/04 17:22:45 by mzutter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+static bool	is_redir(t_token *t)
+{
+	return (t && (t->type == IN || t->type == OUT || t->type == APPEND));
+}
+
+static bool	is_fd_case(t_token *t)
+{
+	return (t->next && (t->next->type == IN || t->next->type == OUT
+			|| t->next->type == APPEND || t->next->type == HDOC)
+		&& t->rank == t->next->rank && is_digit(t->val));
+}
+
+void	set_word_type(t_token *t, t_token *p, bool *is_cmd)
+{
+	if (p && is_redir(p))
+		t->type = FILEN;
+	else if (p && p->type == HDOC)
+		t->type = LIMITER;
+	else if (is_fd_case(t))
+		t->type = FD;
+	else if (!(*is_cmd))
+	{
+		t->type = CMD;
+		*is_cmd = true;
+	}
+	else
+		t->type = ARG;
+}
+
+void	refine_token_type(t_token *token)
+{
+	t_token	*t;
+	t_token	*p;
+	bool	is_cmd;
+
+	t = token;
+	p = NULL;
+	is_cmd = false;
+	while (t)
+	{
+		if (t->type == PIPE)
+			is_cmd = false;
+		else if (t->type == WORD)
+			set_word_type(t, p, &is_cmd);
+		p = t;
+		t = t->next;
+	}
+}
